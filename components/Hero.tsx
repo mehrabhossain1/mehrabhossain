3fa4a3a0aab3ff9dd3f9b81d0a30e9eff3ba8595
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-12-08 19:07:51 +0600

    uploaded assets

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,7 +42,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Mehrab, a Frontend Developer based in Bangladesh.
           </p>
 
           <a href="#about">
