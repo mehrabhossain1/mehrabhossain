3faadc2a0164e1f5aee92ddbededd6241192c936
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-12-10 18:52:58 +0600

    Footer

--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/mehrabhossain1",
   },
   {
     id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/mehrabhossain1",
   },
 ];
